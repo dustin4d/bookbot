4dcdd4583bbedd8877800897ecf108d0aeea9547
Dustin H <DustinHolder14@yahoo.com>
2025-09-26 18:05:24 -0500

    prints the contents of file passed in to console

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello, world!")
-print("greetings boots")
+def get_book_text(file):
+    with open("books/frankenstein.txt") as f:
+        contents_as_string = f.read()
+
+    return contents_as_string
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+
+main()
