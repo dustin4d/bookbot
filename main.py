58c886d1f90fb7ee71e3bfd6168b039a82016c62
Dustin H <DustinHolder14@yahoo.com>
2025-09-26 23:14:03 -0500

    TODO: fix the split to words and not chars

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+book_text = None
+book_words = []
+
 def get_book_text(file):
     with open("books/frankenstein.txt") as f:
-        contents_as_string = f.read()
+        book_text = f.read()
 
-    return contents_as_string
+    return book_text
+
+# TODO fix this function
+# it currently splits the text down by character
+# it SHOULD split based on WORD
+def word_count(book_text):
+    num_words = 0
+    splitted_text = book_text.split(' ')
+    for word in book_text:
+        book_words.append(word)
+        num_words += 1
+    print(book_words)
+
+    return f"Found {num_words} total words"
+
+
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+#    This prints the whole book
+#    print(get_book_text("./books/frankenstein.txt"))
+
+    # this prints the word count
+    print(word_count(get_book_text("./books/frankenstein.txt")))
 
 
 main()
