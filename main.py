d678609b07cd93e56024c120236304abf5e83403
Dustin H <DustinHolder14@yahoo.com>
2025-09-28 22:09:06 -0500

    CH3 L3 Complete, PROJECT COMPLETE

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import sys
 from stats import word_count
 from stats import char_count
 from stats import sort_char_counts
 
+def main():
+
+    total_args = len(sys.argv)
+    if total_args < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else: 
+        filename = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filename}")
+        print("----------- Word Count ----------")
+        print(word_count(get_book_text(filename)))
+        print("--------- Character Count -------")
+        print(sort_char_counts(char_count(get_book_text(filename))))
+
 def get_book_text(file):
-    with open("books/frankenstein.txt") as f:
+    with open(file) as f:
         book_text = f.read()
 
     return book_text
-
-def main():
-
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(word_count(get_book_text("./books/frankenstein.txt")))
-    print("--------- Character Count -------")
-    print(sort_char_counts(char_count(get_book_text("./books/frankenstein.txt"))))
 
 main()
